4e188e1899c9c781f66ae164e0bb4f3dc9d4aa7c
josecanocuadra26-creator <josecanocuadra26@gmail.com>
2026-06-16 09:31:14 +0200

    Inicio Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <div className="container">
+        <h1>Publicacion React en Netlify con github</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
