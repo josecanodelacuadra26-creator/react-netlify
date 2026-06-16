5e4cda05f2d394c6656bde54c8cf0c96090c480d
josecanocuadra26-creator <josecanocuadra26@gmail.com>
2026-06-16 10:05:17 +0200

    agrego maravilla

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <div className="container">
         <h1>Publicacion React en Netlify con github</h1>
         <h2> me gusta mucho React</h2>
+        <h3>Esto es párrafo maravilloso</h3>
       </div>
     </>
   )
